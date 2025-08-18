20a249c5dec212afb702ead4617e9f5bb9c2f35b
kriskilsby <151669187+kriskilsby@users.noreply.github.com>
2025-08-18 20:59:17 +0100

    Deleting some unused code and last working copy before added extended datatable to answer further ecologist query for presentation and demo

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# models/ Directory - This directory defines your database models (if using an ORM like SQLAlchemy).
-# Purpose: Initializes the database models.
-# Example code below
-#from flask_sqlalchemy import SQLAlchemy
-
-#db = SQLAlchemy()
